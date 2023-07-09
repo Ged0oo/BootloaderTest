908ca96f3fe255d03733b90d07832dbc22d02d3a
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-07-09 16:28:42 +0300

    Add files via upload

--- Core/Src/bootloader.c
+++ Core/Src/bootloader.c
@@ -2,24 +2,41 @@
 
 static uint8_t BL_Host_Buffer[BL_HOST_BUFFER_RX_LENGTH];
 
-/*  Last Change  */
+
+static void Bootloader_Get_Version(uint8_t *Host_Buffer);
+static void Bootloader_Get_Help(uint8_t *Host_Buffer);
+static void Bootloader_Get_Chip_Identification_Number(uint8_t *Host_Buffer);
+static void Bootloader_Read_Protection_Level(uint8_t *Host_Buffer);
+static void Bootloader_Jump_To_Address(uint8_t *Host_Buffer);
+static void Bootloader_Erase_Flash(uint8_t *Host_Buffer);
+static void Bootloader_Memory_Write(uint8_t *Host_Buffer);
+static void Bootloader_Enable_RW_Protection(uint8_t *Host_Buffer);
+static void Bootloader_Memory_Read(uint8_t *Host_Buffer);
+static void Bootloader_Get_Sector_Protection_Status(uint8_t *Host_Buffer);
+static void Bootloader_Read_OTP(uint8_t *Host_Buffer);
+static void Bootloader_Change_Read_Protection_Level(uint8_t *Host_Buffer);
+
 
 void BL_Print_Message(char *format, ...)
 {
 	char message[100] = {0};
 
 	va_list args;
-	va_start(args , format);
+	va_start(args, format);
 	vsprintf(message , format , args);
 
 #if (BL_DEBUG_METHOD==BL_ENABLE_UART_DEBUG_MESSAGE)
-	HAL_UART_Transmit(BL_DEBUG_UART , (uint8_t *)message , sizeof(message) , HAL_MAX_DELAY);
+	HAL_UART_Transmit(BL_DEBUG_UART , (uint8_t *)message , sizeof(message), HAL_MAX_DELAY);
+
 #elif (BL_DEBUG_METHOD==BL_ENABLE_SPI_DEBUG_MESSAGE)
 			/*SEND DATA THROUGH SPI*/
+
 #elif (BL_DEBUG_METHOD==BL_ENABLE_I2C_DEBUG_MESSAGE)
 			/*SEND DATA THROUGH I2C*/
+
 #elif (BL_DEBUG_METHOD==BL_ENABLE_CAN_DEBUG_MESSAGE)
 			/*SEND DATA THROUGH CAN*/
+
 #endif
 
 	va_end(args);
@@ -28,6 +45,7 @@ void BL_Print_Message(char *format, ...)
 
 BL_Status BL_UART_Fetch_Host_Command(void)
 {
+	BL_Print_Message("\rBootloader Fetch\r");
 	BL_Status Status = BL_NACK;
 	HAL_StatusTypeDef HAL_Status = HAL_ERROR;
 	uint8_t dataLength = 0;
@@ -38,43 +56,176 @@ BL_Status BL_UART_Fetch_Host_Command(void)
 
 	/* Get length of Command Packet from the HOST */
 	HAL_Status = HAL_UART_Receive(BL_HOST_COMMUNICATION_UART, BL_Host_Buffer, 1, HAL_MAX_DELAY);
-	if(HAL_Status != HAL_OK)
-	{
-		Status = BL_NACK;
-	}
+
+	if(HAL_Status != HAL_OK) Status = BL_NACK;
 	else
 	{
 		dataLength = BL_Host_Buffer[0]-'0';
-		BL_Print_Message("Command Length : %d", dataLength);
+		BL_Print_Message("Command Length : %d \r\n", dataLength);
 		/* Get Command packet from the HOST */
 		HAL_Status = HAL_UART_Receive(BL_HOST_COMMUNICATION_UART, &(BL_Host_Buffer[1]), dataLength, HAL_MAX_DELAY);
-		if(HAL_Status != HAL_OK)
-		{
-			Status = BL_NACK;
-		}
+
+		if(HAL_Status != HAL_OK) Status = BL_NACK;
 		else
 		{
 			_command = BL_Host_Buffer[1] - '0';
-			BL_Print_Message("Command ID : %d", _command);
+			BL_Print_Message("Command ID : %d \r\n", _command);
 			switch(_command)
 			{
-				case CBL_GET_VER_CMD :
-				BL_Print_Message("CBL_GET_VER_CMD Recieved\r\n");
+			case CBL_GET_VER_CMD:
+				BL_Print_Message("CBL_GET_VER_CMD\r\n");
+				Bootloader_Get_Version(BL_Host_Buffer);
+				Status = BL_ACK;
 				break;
 
-				case CBL_GET_HELP_CMD :
-				BL_Print_Message("CBL_GET_HELP_CMD Recieved\r\n");
+			case CBL_GET_HELP_CMD:
+				BL_Print_Message("CBL_GET_HELP_CMD\r\n");
+				Bootloader_Get_Help(BL_Host_Buffer);
+				Status = BL_ACK;
 				break;
 
-				case CBL_GET_CID_CMD :
-				BL_Print_Message("CBL_GET_CID_CMD Recieved\r\n");
+			case CBL_GET_CID_CMD:
+				BL_Print_Message("CBL_GET_CID_CMD\r\n");
+				Bootloader_Get_Chip_Identification_Number(BL_Host_Buffer);
+				Status = BL_ACK;
 				break;
 
-				default :
-				BL_Print_Message("Invalid Code Command Recieved From The Host! \r\n");
+			case CBL_GET_RDP_STATUS_CMD:
+				BL_Print_Message("CBL_GET_RDP_STATUS_CMD\r\n");
+				Bootloader_Read_Protection_Level(BL_Host_Buffer);
+				Status = BL_ACK;
+				break;
+
+			case CBL_GO_TO_ADDR_CMD:
+				BL_Print_Message("CBL_GO_TO_ADDR_CMD\r\n");
+				Bootloader_Jump_To_Address(BL_Host_Buffer);
+				Status = BL_ACK;
+				break;
+
+			case CBL_FLASH_ERASE_CMD:
+				BL_Print_Message("CBL_FLASH_ERASE_CMD\r\n");
+				Bootloader_Erase_Flash(BL_Host_Buffer);
+				Status = BL_ACK;
+				break;
+
+			case CBL_MEM_WRITE_CMD:
+				BL_Print_Message("CBL_MEM_WRITE_CMD\r\n");
+				Bootloader_Memory_Write(BL_Host_Buffer);
+				Status = BL_ACK;
+				break;
+
+			case CBL_ED_W_PROTECT_CMD:
+				BL_Print_Message("CBL_ED_W_PROTECT_CMD\r\n");
+				BL_Print_Message("Enable or Disable write protect on different sectors of the user flash \r\n");
+				Bootloader_Enable_RW_Protection(BL_Host_Buffer);
+				Status = BL_ACK;
+				break;
+
+			case CBL_MEM_READ_CMD:
+				BL_Print_Message("CBL_MEM_READ_CMD\r\n");
+				BL_Print_Message("Read data from different memories of the microcontroller \r\n");
+				Bootloader_Memory_Read(BL_Host_Buffer);
+				Status = BL_ACK;
+				break;
+
+			case CBL_READ_SECTOR_STATUS_CMD:
+				BL_Print_Message("CBL_READ_SECTOR_STATUS_CMD\r\n");
+				BL_Print_Message("Read all the sector protection status \r\n");
+				Bootloader_Get_Sector_Protection_Status(BL_Host_Buffer);
+				Status = BL_ACK;
+				break;
+
+			case CBL_OTP_READ_CMD:
+				BL_Print_Message("CBL_OTP_READ_CMD\r\n");
+				BL_Print_Message("Read the OTP contents \r\n");
+				Bootloader_Read_OTP(BL_Host_Buffer);
+				Status = BL_ACK;
+				break;
+
+			case CBL_CHANGE_ROP_Level_CMD:
+				BL_Print_Message("CBL_CHANGE_ROP_Level_CMD\r\n");
+				Bootloader_Change_Read_Protection_Level(BL_Host_Buffer);
+				Status = BL_ACK;
+				break;
+
+			default:
+				BL_Print_Message("Invalid command code received from host !! \r\n");
 				break;
 			}
 		}
 	}
 	return Status;
+}
+
+
+
+static void Bootloader_Get_Version(uint8_t *Host_Buffer)
+{
+
+}
+
+
+static void Bootloader_Get_Help(uint8_t *Host_Buffer)
+{
+
+}
+
+
+static void Bootloader_Get_Chip_Identification_Number(uint8_t *Host_Buffer)
+{
+
+}
+
+
+static void Bootloader_Read_Protection_Level(uint8_t *Host_Buffer)
+{
+
+}
+
+
+static void Bootloader_Jump_To_Address(uint8_t *Host_Buffer)
+{
+
+}
+
+
+static void Bootloader_Erase_Flash(uint8_t *Host_Buffer)
+{
+
+}
+
+
+static void Bootloader_Memory_Write(uint8_t *Host_Buffer)
+{
+
+}
+
+
+static void Bootloader_Enable_RW_Protection(uint8_t *Host_Buffer)
+{
+
+}
+
+
+static void Bootloader_Memory_Read(uint8_t *Host_Buffer)
+{
+
+}
+
+
+static void Bootloader_Get_Sector_Protection_Status(uint8_t *Host_Buffer)
+{
+
+}
+
+
+static void Bootloader_Read_OTP(uint8_t *Host_Buffer)
+{
+
+}
+
+
+static void Bootloader_Change_Read_Protection_Level(uint8_t *Host_Buffer)
+{
+
 }
